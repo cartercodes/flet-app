2ef57eb7f31acc0de524de9824d6fb49a83b1e2c
cartercodes <carter.svo@gmail.com>
2024-04-12 17:41:32 -0400

    Adding previous days code. Created new containers and router.

--- main.py
+++ main.py
@@ -6,25 +6,96 @@ def main (page: Page):
     FG = '#f1f7ed'
     PINK = '#e8998d' 
 
+    def route_change(route):
+        page.views.clear()
+        page.views.append(
+            pages[page.route]
+        )
+
+
+    categories_card = Row(
+        scroll='auto',
+    )
+
+    create_task_view = Container(
+        content=Container(on_click=lambda e: page.go('/'),
+            height=40, width=40,
+            content=Text('x')          
+        )
+    )
+
+    tasks=Column()
+
+    categories = ['Business', 'Family', 'Study', 'Health']
+    for i, category in enumerate(categories):
+        categories_card.controls.append(
+            Container(
+                border_radius=20.
+                bgcolor=BG,
+                width=170,
+                height=110,
+                padding=15,
+                content=Column(
+                    controls=[
+                        Text('40 Tasks')
+                        Text(category),
+                        Container(
+                            width=160,
+                            height=5,
+                            bgcolor='white12'
+                            border_radius=20,
+                            padding=padding.only(right=i*30),
+                            content=Container(
+                                bgcolor=PINK,
+                            )
+
+                        )
+                    ]
+                )
+            )
+        )
+
     first_page_contents = Container(
         content=Column(
             controls=[
-                Row( alignment='spaceBetween', # spaces the children below. 
+                Row(alignment='spaceBetween',
+                controls=[
+                    Container(
+                    on_click=lambda e: shrink(e),
+                    content=Icon(
+                        icons.MENU)),
+                    Row(
                     controls=[
-                        Container(
-                            content=Icon(
-                                icons.Menu)),
-                        Row(
-                            controls=[
-                                Icon(icons.SEARCH),
-                                Icon(icons.NOTIFICATIONS_OUTLINED)
-                            ]
-                        )
-                    ]       
+                        Icon(icons.SEARCH),
+                        Icon(icons.NOTIFICATIONS_OUTLINED)
+                    ],
+                    ),
+                ],
+                ),
+                Container(height=20),
+                Text(
+                value='What\'s up, Olivia!'
+                ),
+                Text(
+                value='CATEGORIES'
+                ),
+                Container(
+                padding=padding.only(top=10,bottom=20,),
+                content=categories_card
+                ),
+                Container(height=20),
+                Text("TODAY'S TASKS"),
+                Stack(
+                controls=[
+                    tasks,
+                    FloatingActionButton(bottom=2,right=20,
+                    icon = icons.ADD,on_click=lambda _: page.go('/create_task') #floating action button
+                    )
+                ]
                 )
-            ]
-        )
-    )       
+            ],
+        ),
+    )    
 
     page_1 = Container()
 
@@ -62,6 +133,25 @@ def main (page: Page):
             ]
         )
     )
+
+    #dictionary of pages
+    pages = {
+        '/' : View(
+            "/",
+            [
+                container
+            ],
+        ),
+        '/create_task' : View(
+                        "/create_task",
+                        [
+                            create_task_view
+                        ],
+        )
+    }
+
     page.add(container)
+    page.on_route_change = route_change #router 
+    page.go(page.route)
 
 app(target=main)
